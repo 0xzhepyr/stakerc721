2eebb24a3bbed4fc5a0ab1705647f6b45622040d
0xzhepyr <0xzhepyr@gmail.com>
2024-04-19 17:35:37 +0700

    upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0xD122B202b20332819A958C686bf03c9272f0d49B";
+export const tokenContractAddress =
+  "0xe10B1e5b9DC8674c903deb18E27a64A1dCFbF342";
+export const stakingContractAddress =
+  "0x57F685419bf25907eeF31775f0774E096E902701";
